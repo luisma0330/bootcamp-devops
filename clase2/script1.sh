9b122bcdf420d03c90146f0f80480b5a43fc6fab
Luis Ojeda <luisma0330@hotmail.com>
2023-11-08 00:48:01 -0600

    Agregando el contenido de la clase 2

---
+++ clase2/script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "este es es mi primer script"
+mkdir hola
+touch hola/prueba.txt
+echo "fin de script"
---
+++ clase2/script3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#variable
+
+VALOR1=$1
+VALOR2=$2
+
+echo "este es es mi primer script de variable $VALOR1"
+#mkdir hola
+#touch hola/prueba.txt
+echo "fin de script variable $VALOR2"
