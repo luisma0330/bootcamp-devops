7235aaa73e7a37debca5f35dfc4c546016724324
luisma0330 <luisma0330@hotmail.com>
2023-11-09 15:36:08 -0600

    cambios al script

--- vagrant/clase2/script1.sh
+++ vagrant/clase2/script1.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-echo "este es es mi primer script"
-mkdir hola
-touch hola/prueba.txt
-echo "fin de script"
+REPO="devops-static-web"
+
+echo "=================="
+
+apt-get update
+
+echo "El servidor esta actualizado"
+
+if dpkg -l | grep -q apache2 ;
+then
+    echo "ya esta instalado apache2"
+else
+    echo "Instalando paquete apache2"
+    apt install apache2 -y
+    systemctl start apache2
+    systemctl enable apache2
+fi
+
+if [ -d "$REPO" ] ;
+then
+    echo "la carpeta $REPO ya exite"
+else
+    git clone -b devops-mariobros https://github.com/roxsross/$REPO.git
+fi
+
+echo "instalando web"
+sleep 1
+
+cp -r $REPO/* /var/www/html
